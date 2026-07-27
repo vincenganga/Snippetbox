81a64273b97e6ff1da436752e0cbd728e55d06ef
Vincent <vmburu21@gmail.com>
2026-07-27 15:55:08 +0300

    Adding snippetView & snippetCreate routes

--- main.go
+++ main.go
@@ -5,26 +5,31 @@ import (
 	"net/http"
 )
 
-// Define a home handler function which writes a byte slice containing
-// "Hello from Snippetbox" as a response body
+// Home handler function 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+// SnippetView handler function
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// SnippetCreate handler function
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
 func main() {
-	// Use the http.NewServerMux() function to initialize a new servemux, then
-	// register the home function as the handler for the "/" URL pattern
+	// http.NewServerMux() function to initialize a new servemux
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	// Print a log message to say that the server is starting
 	log.Print("Starting server on :4000")
 
-	// Use the http.ListenAndServe() function to start a new web server. We pass in
-	// two parameters: the TCP network address to listen on (in this case ":4000")
-	// and the servemux we just created. If http.ListenAndServe() returns an error
-	// we use the log.Fatal() function to log the error message and exit. Note
-	// that any error returned by http.ListenAndServe() is always non-nil.
+	// http.ListenAndServe() function to start a new web server.
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
